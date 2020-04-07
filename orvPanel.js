8a8417a0e0e63bfde89246d9af8d7157877b0182
Orville Chomer <orville.chomer@gmail.com>
2020-04-07 18:25:49 -0500

    Create orvPanel.js

---
+++ orvPanel.js
@@ -0,0 +1,20 @@
+/*
+   Do a visual "panel"
+   
+   features:
+     - panel acts as a Container for other visual elements
+     - Configurable:
+       - panel that "sticks" to top/bottom/left/right of open area of window
+       - panels can be stacked
+       - ability to become a child "window"
+       - ability to be resizeable via mouse
+       - ability to make panel closable.
+       - ability to hide/show panel.
+       - set the panel's caption.
+       - ability to visually style the panel (CSS)
+       - panels can have their own title bar / status bar.
+       - import/export configuration data
+     - Events:
+       - resize,open,close,show,hide,contentChanged
+     
+ */
