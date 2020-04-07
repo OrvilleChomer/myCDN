44233db3c866f77026c230fff90e788d6d7cd2a9
Orville Chomer <orville.chomer@gmail.com>
2020-04-07 18:09:49 -0500

    Create orvHeroBanner.js

---
+++ orvHeroBanner.js
@@ -0,0 +1,24 @@
+/*
+
+
+    params at this point...
+    
+    id:       (string) the id of Div to place hero banner in (or...)
+    node:     DOM node to put hero banner in
+    title:    (string)Main title 
+    subtitle: (string)Sub Title
+    imageUrl: (string) Main Banner Image (would be bottom-most layer if doing paralax effects)
+    imageLayers: (array) Strings of Urls. Used for paralax effects
+    genTestBanner: (boolean)
+    theme: (string) Name of style theme (either from a list of presets or a custom theme by the dev)
+    callToAction: {
+      caption: (string) call to action button's caption
+      url: (string) Url to go to if call to action button is clicked (or...)
+      jsFunction: (function) JS function to call if call to action button is clicked
+       }
+ */
+ 
+ 
+ function makeHeroBanner(params) {
+ } // end of function
+ 
